eb8648374895d6f5667b464f04b7015fb0cc607a
ddasgupta <debangshu.dasgupta@gmail.com>
2026-01-17 22:56:34 -0500

    Initial code cleanup

--- openai-chat-api/src/main/java/org/doodle/ai/openai/chat/api/OpenaiChatApiApplication.java
+++ openai-chat-api/src/main/java/org/doodle/ai/openai/chat/api/OpenaiChatApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OpenaiChatApiApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(OpenaiChatApiApplication.class, args);
     }
 

d5fd13a6d701f95b67e8c03001b341aa0cb61313
ddasgupta <debangshu.dasgupta@gmail.com>
2026-01-17 23:02:13 -0500

    Added ChatMemoryAdvisor and SimpleLoggerAdvisor

--- openai-chat-api/src/main/java/org/doodle/ai/openai/chat/api/OpenaiChatApiApplication.java
+++ openai-chat-api/src/main/java/org/doodle/ai/openai/chat/api/OpenaiChatApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OpenaiChatApiApplication {
-
     static void main(String[] args) {
         SpringApplication.run(OpenaiChatApiApplication.class, args);
     }
-
 }
--- openai-chat-api/src/main/java/org/doodle/ai/openai/chat/api/config/ChatConfig.java
+++ openai-chat-api/src/main/java/org/doodle/ai/openai/chat/api/config/ChatConfig.java
@@ -1,6 +1,9 @@
 package org.doodle.ai.openai.chat.api.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class ChatConfig {
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
         return chatClientBuilder
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
     }
+
+
 }
